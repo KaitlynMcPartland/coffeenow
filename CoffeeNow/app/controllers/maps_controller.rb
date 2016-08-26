e734362c6eadd6080aeb8d9dc31f5af57092ac69
macroscopic <mac.roche@gmail.com>
2016-08-26 14:38:09 -0700

    Add API call and formatting to pages

--- CoffeeNow/app/controllers/maps_controller.rb
+++ CoffeeNow/app/controllers/maps_controller.rb
@@ -5,34 +5,26 @@ class MapsController < ApplicationController
   end
 
   def show
-    puts "Hello World from the Show Page"
-    puts "***" *100
-    p geolocation
+    @lat = geolocation['location']['lat']
+    @long = geolocation['location']['lng']
+    @fearless_lat = "37.785205"
+    @fearless_long = "-122.395468"
+    @place_id = places
   end
 
   def geolocation
-
-    headers = {
-      "key" => ENV['GEO_API_KEY']
-    }
-
-    geo_response = HTTParty.post(
-      "https://www.googleapis.com/geolocation/v1/geolocate",
-      headers: headers)
-    geo_response
-    
+    response = HTTParty.post("https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV['GEO_API_KEY']}")
   end
 
   def places
-    @lat = geolocation
-    # @long = geolocation['location']['long']
-    p @lat
-    p @long
+    @lat = geolocation['location']['lat']
+    @long = geolocation['location']['lng']
+    response = HTTParty.post("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@lat},#{@long}&radius=500&type=cafe&key=#{ENV['PLACES_API_KEY']}")
+    return response['results'][1]['id']
   end
 
   def directions
-    @lat = geo_response['location']['lat']
-    @long = geo_response['location']['long']
+    @place_id = places
   end
 
 end
