3901b04e15c5bf7e7fb9524ac3740604e46275d8
macroscopic <mac.roche@gmail.com>
2016-08-25 23:42:59 -0700

    Begin adding Google map functionality

--- CoffeeNow/app/controllers/maps_controller.rb
+++ CoffeeNow/app/controllers/maps_controller.rb
@@ -6,6 +6,33 @@ class MapsController < ApplicationController
 
   def show
     puts "Hello World from the Show Page"
+    puts "***" *100
+    p geolocation
+  end
+
+  def geolocation
+
+    headers = {
+      "key" => ENV['GEO_API_KEY']
+    }
+
+    geo_response = HTTParty.post(
+      "https://www.googleapis.com/geolocation/v1/geolocate",
+      headers: headers)
+    geo_response
+    
+  end
+
+  def places
+    @lat = geolocation
+    # @long = geolocation['location']['long']
+    p @lat
+    p @long
+  end
+
+  def directions
+    @lat = geo_response['location']['lat']
+    @long = geo_response['location']['long']
   end
 
 end
--- CoffeeNow/app/helpers/maps_helper.rb
+++ CoffeeNow/app/helpers/maps_helper.rb
@@ -1,2 +1,5 @@
 module MapsHelper
+
+
+  
 end
