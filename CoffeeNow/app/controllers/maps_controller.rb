0b770a4b56b2a97dc905bf2fa705aa24b141a3a9
KaitlynMcPartland <kmcp921@gmail.com>
2016-10-05 17:40:41 -0700

    Remove hard coded address and set up map so it displays directions to show nearest to current location yay

--- CoffeeNow/app/controllers/maps_controller.rb
+++ CoffeeNow/app/controllers/maps_controller.rb
@@ -7,20 +7,32 @@ class MapsController < ApplicationController
   def show
     @lat = geolocation['location']['lat']
     @long = geolocation['location']['lng']
+    p "*" * 200
+    p @lat
+    p @long
     @fearless_lat = "37.785205"
     @fearless_long = "-122.395468"
-    @place_id = places
+    @place_id = places['id']
+    @place_name = places['name']
+    @place_lat = places['geometry']['location']['lat']
+    @place_long = places['geometry']['location']['lng']
+    p "%" * 200
+    puts places
+    puts @place_lat
   end
 
+#this is making a call to get the location of the user
   def geolocation
+    p "$" * 100
     response = HTTParty.post("https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV['GEO_API_KEY']}")
   end
 
+# get the place ID of the nearest open coffee shop to current location
   def places
     @lat = geolocation['location']['lat']
     @long = geolocation['location']['lng']
     response = HTTParty.post("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@lat},#{@long}&radius=500&type=cafe&key=#{ENV['PLACES_API_KEY']}")
-    return response['results'][1]['id']
+    return response['results'][1]
   end
 
   def directions
